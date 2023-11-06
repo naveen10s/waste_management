9fc0804824599a93058af74c8f8bc11144f62068
naveen10s <naveens10102002>
2023-11-06 17:36:10 +0530

    testone

---
+++ waste_management/waste_management/doctype/web_setting/test_web_setting.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Neveka and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testwebsetting(FrappeTestCase):
+	pass
---
+++ waste_management/waste_management/doctype/web_setting/web_setting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Neveka and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('web setting', {
+	// refresh: function(frm) {
+
+	// }
+});
